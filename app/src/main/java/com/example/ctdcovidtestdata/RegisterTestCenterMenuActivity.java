da7fecd7c17817705dc0f6610cb38d1d53466bd2
Shery <50106719+sherychenz@users.noreply.github.com>
2021-02-22 17:48:48 +0800

    Design Register Test Center

---
+++ app/src/main/java/com/example/ctdcovidtestdata/RegisterTestCenterMenuActivity.java
@@ -0,0 +1,14 @@
+package com.example.ctdcovidtestdata;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_test_center_menu);
+    }
+}
