3cb8ec6c38c05ae895931f66b451858889d1c44b
Shery <50106719+sherychenz@users.noreply.github.com>
2021-02-23 14:15:25 +0800

    Design Test Center Manager Menu

--- app/src/main/java/com/example/ctdcovidtestdata/TestCenterManagerMenuActivity.java
+++ app/src/main/java/com/example/ctdcovidtestdata/TestCenterManagerMenuActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainMenuActivity extends AppCompatActivity {
+public class TestCenterManagerMenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register_test_center_menu);
+        setContentView(R.layout.activity_test_center_manager_menu);
     }
 }
