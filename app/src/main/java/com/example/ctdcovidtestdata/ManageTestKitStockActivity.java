4c05205fc26aae0c244b8638ddaad6f4f35953ed
Shery <50106719+sherychenz@users.noreply.github.com>
2021-02-23 17:24:35 +0800

    Design Manage Kit Stock

---
+++ app/src/main/java/com/example/ctdcovidtestdata/ManageTestKitStockActivity.java
@@ -0,0 +1,14 @@
+package com.example.ctdcovidtestdata;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManageTestKitStockActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_test_kit_stock);
+    }
+}
