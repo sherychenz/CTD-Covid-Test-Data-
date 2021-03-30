72cd1481c6c36fa97e5fb00e814e08d881478987
Shery <50106719+sherychenz@users.noreply.github.com>
2021-03-30 15:54:30 +0800

    Register Java

---
+++ app/src/main/java/com/example/ctdcovidtestdata/user.java
@@ -0,0 +1,58 @@
+package com.example.ctdcovidtestdata;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class user {
+
+    private static final String URL_REGISTER = ;
+
+    public void register(Context context , String name, String phone, String email, String address, String username, String password){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_REGISTER,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                    } catch (JSONException e){
+                            e.printStackTrace();
+                            Toast.makeText(context, "Invalid Username or Password", Toast.LENGTH_SHORT).show();
+                        }
+                     }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
+                }
+        ) {
+        @Override
+        protected Map<String, String> getParams() throws AuthFailureError{
+            Map<String, String> params = new HashMap<>();
+            params.put("username", username);
+            params.put("name", name);
+            params.put("phone", phone);
+            params.put("address", address);
+            params.put("email", email);
+            params.put("password", password);
+            return params;
+        }
+    };
+    RequestQueue requestQueue = Volley.newRequestQueue(context);
+    requestQueue.add(stringRequest);
+    }
+}
