0b5d6c3b204a0a369782ebe17865caec674be670
ivandynezha <79440876+ivandynezha@users.noreply.github.com>
2021-03-31 15:40:45 +0800

    update

--- app/src/main/java/com/example/ctdcovidtestdata/RegisterCenterMenuActivity.java
+++ app/src/main/java/com/example/ctdcovidtestdata/RegisterCenterMenuActivity.java
@@ -1,31 +1,14 @@
 package com.example.ctdcovidtestdata;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-
 public class RegisterCenterMenuActivity extends AppCompatActivity {
-    private EditText userNameText, nameText, phoneText, addressText, emailText, passwordText;
-    private String UserName,Name,Phone,Address,Email,Password;
-
-    user user = new user();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_center_menu);
-
-        userNameText    = findViewById(R.id.UserName);
-        nameText        = findViewById(R.id.Name);
-        phoneText       = findViewById(R.id.Phone);
-        addressText     = findViewById(R.id.Address);
-        emailText       = findViewById(R.id.Email);
-        passwordText    = findViewById(R.id.Password);
-
-    }
-    public void Register(View view) {
-
     }
 }
