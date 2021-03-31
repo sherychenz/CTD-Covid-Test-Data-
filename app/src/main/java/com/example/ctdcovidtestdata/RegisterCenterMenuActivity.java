85522a612f236b25311ff179f92fa56a43f1ff7a
Shery <50106719+sherychenz@users.noreply.github.com>
2021-03-31 15:54:23 +0800

    Register Java edit

--- app/src/main/java/com/example/ctdcovidtestdata/RegisterCenterMenuActivity.java
+++ app/src/main/java/com/example/ctdcovidtestdata/RegisterCenterMenuActivity.java
@@ -1,14 +1,26 @@
 package com.example.ctdcovidtestdata;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterCenterMenuActivity extends AppCompatActivity {
+    private EditText UserNameText, NameText, EmailText, PhoneText, AddressText, PasswordText;
+    private String UserName, Name, Email, Phone, Address, Password;
+
+    user user = new user();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_center_menu);
+
+        UserNameText        = findViewById(R.id.UserName);
+        NameText            = findViewById(R.id.Name);
+        EmailText           = findViewById(R.id.Email);
+        PhoneText           = findViewById(R.id.Phone);
+        AddressText         = findViewById(R.id.Address);
+        PasswordText        = findViewById(R.id.Password);
     }
 }
