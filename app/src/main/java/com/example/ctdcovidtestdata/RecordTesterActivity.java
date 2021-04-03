2fa987b27eba93f68fd1cac726352a02174b1379
ivandynezha <79440876+ivandynezha@users.noreply.github.com>
2021-04-03 22:17:39 +0800

    Record Tester Java

--- app/src/main/java/com/example/ctdcovidtestdata/RecordTesterActivity.java
+++ app/src/main/java/com/example/ctdcovidtestdata/RecordTesterActivity.java
@@ -1,17 +1,39 @@
 package com.example.ctdcovidtestdata;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class RecordTesterActivity extends AppCompatActivity {
+
+    private EditText UserNameText, NameText, PhoneText, AddressText, PasswordText;
+    private String UserName, Name, Phone, Address, Password;
+
+    user user = new user();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record_tester);
+
+        UserNameText        = findViewById(R.id.UserName);
+        NameText            = findViewById(R.id.Name);
+        PhoneText           = findViewById(R.id.Phone);
+        AddressText         = findViewById(R.id.Address);
+        PasswordText        = findViewById(R.id.Password);
+    }
+
+    public void RecordTester(View view) {
+        UserName = UserNameText.getText().toString().trim();
+        Name = NameText.getText().toString().trim();
+        Phone = PhoneText.getText().toString().trim();
+        Address = AddressText.getText().toString().trim();
+        Password = PasswordText.getText().toString().trim();
+
+        user.register(getApplicationContext(), Name, Phone, Address, UserName, Password);
     }
 
     public void BackMenu(View view) {
