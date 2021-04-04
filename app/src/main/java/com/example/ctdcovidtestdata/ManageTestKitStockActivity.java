ea7039b7af8ba3232016614e1f47265dc4350872
Shery <50106719+sherychenz@users.noreply.github.com>
2021-04-04 19:55:09 +0800

    Add New Test Kit

--- app/src/main/java/com/example/ctdcovidtestdata/ManageTestKitStockActivity.java
+++ app/src/main/java/com/example/ctdcovidtestdata/ManageTestKitStockActivity.java
@@ -5,13 +5,64 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class ManageTestKitStockActivity extends AppCompatActivity {
+
+    private static final String URL_testkitdata = "https://ctd5758.000webhostapp.com/testkitdata.php";
+
+    ArrayList<String>TestKit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_test_kit_stock);
+
+        TestKit = new ArrayList<>();
+        getData(TestKit);
+
+    }
+
+    public void getData(ArrayList<String> testKitArray){
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, URL_testkitdata,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        System.out.println(response);
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            JSONArray testkitarray = jsonObject.getJSONArray("Testkit");
+                            for (int i = 0; i < testkitarray.length(); i++) {
+                                JSONObject testkitobject = testkitarray.getJSONObject(i);
+                                testKitArray.add(testkitobject.getString("TestKitName"));
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(getApplicationContext(), "Human Error", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        requestQueue.add(stringRequest);
     }
 
     public void BackMenu(View view) {
@@ -22,7 +73,6 @@ public class ManageTestKitStockActivity extends AppCompatActivity {
 
     public void AddNewKkit(View view) {
         Intent intent = new Intent(getApplicationContext(), NewTestKitActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
     }
 }
